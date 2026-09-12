170e0422098ff6affc965d8c1ffc8c88edcf71a3
Andres Salinas <andresali1@hotmail.es>
2026-09-12 15:19:20 +0200

    Dockerizing Account.Api

---
+++ src/Account/Account.Infrastructure/Persistence/AccountDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Account.Infrastructure.Persistence
+{
+    internal class AccountDbContext
+    {
+    }
+}
---
+++ src/Account/Account.Infrastructure/Repositories/AccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Account.Infrastructure.Repositories
+{
+    internal class AccountRepository
+    {
+    }
+}
